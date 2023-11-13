56d39cdd9099fecd1723edec3026cef3eb1ef268
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-11-13 14:33:05 +0200

    Refactored process function

--- c/src/process_directory.c
+++ c/src/process_directory.c
@@ -11,6 +11,44 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+FILETYPE do_statistics(const char* source_dir_path, 
+                       const char* destination_dir_path, 
+                       const char* entry_name) {
+    
+    FILETYPE filetype = ERROR;
+
+    char output_file[500], file_path[500];
+    sprintf(file_path, "%s%s", source_dir_path, entry_name);
+    sprintf(output_file, "%s%s-statistics.txt", destination_dir_path, entry_name);
+
+    int fd_statistics = creat(output_file, S_IRUSR | S_IWUSR);
+    if(fd_statistics == -1) {
+        return ERROR;
+    }
+    if((filetype = write_statistics(fd_statistics, file_path)) == ERROR) {
+        close(fd_statistics);
+        return ERROR;
+    }
+
+    if(close(fd_statistics) == -1) {
+        return ERROR;
+    }
+    return filetype;
+}
+
+// Returns the number of lines written to the output file
+int number_of_statistics_lines(FILETYPE filetype) {
+    switch(filetype) {
+        case BMP_FILE: return 10;
+        case REGULAR_FILE: return 8;
+        case SYMLINK: return 6;
+        case DIRECTORY: return 5;
+        case OTHER: return 0;
+        case ERROR: return -1;
+    }
+    return -2;
+}
+
 bool process_dir(const char *dir_path, const char *output_dir) {
     DIR* directory = opendir(dir_path);
     if(directory == NULL) {
@@ -18,36 +56,17 @@ bool process_dir(const char *dir_path, const char *output_dir) {
     }
 
     struct dirent* entry = NULL;
-    char file_path[500];
-    int status;
+    int status = 0;
 
     while((entry = readdir(directory)) != NULL) {
-        sprintf(file_path, "%s%s", dir_path, entry->d_name);
-
         int pid = 0;
-        if((pid=fork()) < 0) {
+        if((pid = fork()) < 0) {
             closedir(directory);
             return false;
         }
-        if(pid==0) {
-            char output_file[500];
-            sprintf(output_file, "%s%s-statistics.txt", output_dir, entry->d_name);
-
-            int fd_statistics = creat(output_file, S_IRUSR | S_IWUSR);
-            if(fd_statistics == -1) {
-                closedir(directory);
-                return false;
-            }
-            if(write_statistics(fd_statistics, file_path) == ERROR) {
-                close(fd_statistics); closedir(directory);
-                return false;
-            }
-
-            if(close(fd_statistics) == -1) {
-                closedir(directory);
-                return false;
-            }
-            exit(0);
+        if(pid == 0) {
+            FILETYPE filetype = do_statistics(dir_path, output_dir, entry->d_name);
+            exit(number_of_statistics_lines(filetype));
         }
         wait(&status);
     }
